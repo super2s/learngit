b76b3f46f499acd89b7a1ac82796f0e1bccc7899
superyu <1031488054@qq.com>
2019-02-11 15:22:32 +0800

    默认配置

---
+++ config_default.py
@@ -0,0 +1,16 @@
+'''
+默认配置
+'''
+configs = {
+	'debug': True,
+	' db ':{
+		'host':'127.0.0.1',
+		'port':3306,
+		'user':'www-data',
+		'passwrod':'www-data',
+		'database':'awesome'
+	},
+	'session':{#库
+		'secret':'AwEsOmE'
+	}
+}
