ad16c997b9a7de08b2c1bd5ec15cbd0833a117ae
superyu <1031488054@qq.com>
2019-02-13 00:34:05 +0800

    添加mvc

--- handlers.py
+++ handlers.py
@@ -8,8 +8,13 @@ from models import User, Comment, Blog, next_id
 
 @get('/')
 async def index(request):
-    users = await User.findAll()
-    return {
-        '__template__': 'test.html',
-        'users': users
-    }
+	summary = '这是我的第一个网站'
+	blogs =[
+		Blog(id='1', name='python新手',summary=summary,created_at=time.time()-300),
+		Blog(id='2', name='super余的',summary=summary,created_at=time.time()-5000),
+		Blog(id='3', name='测试日志',summary=summary,created_at=time.time()-200000)
+	]
+	return{
+		'__template__': 'blogs.html',
+		'blogs': blogs
+	}
