6424550235a14e15dcf3ab500e135c03d4e3aae8
superyu <1031488054@qq.com>
2019-02-14 23:33:39 +0800

    添加管理员权限

--- handlers.py
+++ handlers.py
@@ -7,7 +7,7 @@ import markdown2
 from aiohttp import web
 
 from coroweb import get, post
-from apis import APIValueError,APIResourceNotFoundError
+from apis import Page,APIValueError,APIResourceNotFoundError
 
 from models import User, Comment, Blog, next_id
 from config import configs
@@ -15,6 +15,19 @@ from config import configs
 COOKIE_NAME = 'awesession'
 _COOKIE_KEY =  configs.session.secret
 
+def check_admin(request):
+	if request.__user__ is None or not request.__user__.admin:
+		raise APIPermissionError()
+
+def get_page_index(page_str):
+	p=1
+	try:
+		p = int(page_str)
+	except ValueError as e:
+		pass
+	if p < 1:
+		p = 1
+	return p
 def user2cookie(user,max_age):
 	'''
 	用户生成cookie字符串
@@ -25,6 +38,9 @@ def user2cookie(user,max_age):
 	L = [user.id,expires,hashlib.sha1(s.encode('utf-8')).hexdigest()]
 	return '-'.join(L)
 
+def text2html(text):
+	lines =map(lambda s: '<p>%s</p>' %s.replace('&','&amp;').replace('<','&lt;').replace('>','&gt'),filter(lambda s:s.strip() != '',text.split('\n')))
+	return ''.join(lines)
 #解密COOKIE
 async def cookie2user(cookie_str):
 	'''
@@ -64,6 +80,19 @@ def index(request):
 		'__template__': 'blogs.html',
 		'blogs': blogs
 	}
+
+@get('/blog/{id}')
+async def get_blog(id):
+	blog = await Blog.find(id)
+	comments = await Comment.findAll('blog_id=?',[id],orderBy='created_at desc')
+	for c in comments:
+		c.html_content = text2html(c.content)
+	blog.html_content = markdown2.markdown(blog.content)
+	return {
+		'__template__': 'blog.html',
+		'blog': blog,
+		'comments': comments
+	}
 	
 @get('/register')
 def register():
@@ -83,7 +112,7 @@ async def authenticate(*,email,passwd):
 		raise APIValueError('email','Invalid email')
 	if not passwd:
 		raise APIValueError('passwd','Invalid password')
-	user = await User.findAll('email=?',[email])
+	users = await User.findAll('email=?',[email])
 	if len(users) == 0:
 		raise APIValueError('email','邮箱不存在.')
 	user = users[0]
@@ -109,7 +138,21 @@ def signout(request):
 	r.set_cookie(COOKIE_NAME,'-deleted-',max_age=0,httponly=True)
 	logging.info('user signed out.')
 	return r
+	
+@get('/manage/blogs')
+def manage_blogs(*,page='1'):
+	return {
+		'__template__': 'manage_blogs.html',
+		'page_index': get_page_index(page)
+	}
 
+@get('/manage/blogs/create')
+def manage_create_blog():
+	return {
+		'__template__': 'manage_blog_edit.html',
+		'id': '',
+		'action': '/api/blogs'
+	}
 _RE_EMAIL = re.compile(r'^[a-z0-9\.\-\_]+\@[a-z0-9\-\_]+(\.[a-z0-9\-\_]+){1,4}$')
 _RE_SHA1 = re.compile(r'^[0-9a-f]{40}$')
 
@@ -117,16 +160,16 @@ _RE_SHA1 = re.compile(r'^[0-9a-f]{40}$')
 async def api_register_user(*,email,name,passwd):
 	if not name or not name.strip():
 		raise APIValueError('name')
-	if not email or not _RE_EMAIL(email):
+	if not email or not _RE_EMAIL.match(email):
 		raise APIValueError('eamil')
 	if not passwd or not _RE_SHA1.match(passwd):
 		raise APIValueError('passwd')
 	users = await User.findAll('email=?',[email])
 	if len(users) > 0:
-		raise APIValueError('注册：失败','邮箱','该邮箱已经在使用中')
+		raise APIValueError('注册：失败','该邮箱已经在使用中')
 	uid = next_id()
 	sha1_passwd = '%s:%s' %(uid,passwd)
-	user = User(id=uid,name=name.strip(),email=email,passwd=hashlib.sha1(sha1_passwd.encode('utf-8')).hexdigest(),image='http://www.gravatar.com/avatar/%s?dd=mm&s=120' %hashlib.md5(email.encode('utf-8')).hexdigest())
+	user = User(id=uid,name=name.strip(),email=email,passwd=hashlib.sha1(sha1_passwd.encode('utf-8')).hexdigest(),image='http://www.gravatar.com/avatar/%s?dd=mm&s=120' %hashlib.md5(email.encode('utf-8')).hexdigest(),admin=True)
 	await user.save()
 	#制作会话cookie
 	r = web.Response()
@@ -135,3 +178,33 @@ async def api_register_user(*,email,name,passwd):
 	r.content_type = 'application/json'
 	r.body = json.dumps(user,ensure_ascii=False).encode('utf-8')
 	return r
+	
+@get('/api/blogs')
+async def api_blogs(*,page='1'):
+	page_index = get_page_index(page)
+	num = await Blog.findNumber('count(id)')
+	p = Page(num,page_index)
+	if num == 0:
+		return dict(page=p, blog=())
+	blogs = await Blog.findAll(orderBy='created_at desc',limit=(p.offset,p.limit))
+	return dict(page=p,blogs=blogs)
+	
+@get('/api/blogs/{id}')
+async def api_get_blog(*,id):
+	blog = await Blog.find(id)
+	return blog
+	
+
+	
+@post('/api/blogs')
+async def api_create_blog(request,*,name,summary,content):
+	check_admin(request)
+	if not name or not name.strip():
+		raise APIValueError('name','name不能为空')
+	if not summary or not summary.strip():
+		raise APIValueError('summary','摘要不能为空')
+	if not content or not content.strip():
+		raise APIValueError('content','内容不能为空')
+	blog = Blog(user_id=request.__user__.id,user_name=request.__user__.name,user_image=request.__user__.image,name=name.strip(),summary=summary.strip(),content=content.strip())
+	await blog.save()
+	return blog
